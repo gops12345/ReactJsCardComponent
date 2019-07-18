c485e1873bbab8f9565c6a35c8fa5bf2ef350147
gops12345 <agrawalgopala@gmail.com>
2019-07-18 16:06:31 +0530

    check

--- src/CardComponent/CardComponent.js
+++ src/CardComponent/CardComponent.js
@@ -1,120 +1,85 @@
-import React, {Component} from 'react';
-import CHead from '../CardHeader/CardHeader';
-import CFoot from '../CardFoot/CardFoot';
-import CChange from '../ColorChange/ColorChange';
-import styles from '../CardHeader/CardHeader.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { Component } from "react";
+import CHead from "../CardHeader/CardHeader";
+import CFoot from "../CardFoot/CardFoot";
+import CChange from "../ColorChange/ColorChange";
+import styles from "../CardHeader/CardHeader.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class Card extends Component{
-    state = {
-        bodyMessage: 'Learn React? Start small.',
-        footerMessage: 'Something Something else i fnot something else',
-        footerHeading: 'Learn React? Start small.',
-        name: 'Gopala Agrawal',
-        clickedRed: false,
-        clickedBlue: false,
-        clickedGreen: false,
-        clickedYellow: false,
+class Card extends Component {
+  state = {
+    bodyMessage: "Learn React? Start small.",
+    footerMessage: "Something Something else i fnot something else",
+    footerHeading: "Learn React? Start small.",
+    name: "Gopala Agrawal",
+    clickedRed: true,
+    clickedBlue: false,
+    clickedGreen: false,
+    clickedYellow: false,
 
-        showColorTab: false
-    }
+    showColorTab: false
+  };
 
-    clickedRedColor = () =>{
-        this.setState({
-            clickedRed : true
-        });
-    }
+  clickedRedColor = () => {
+    this.setState({
+      clickedRed: true
+    });
+  };
 
-    clickedBlueColor = () =>{
-        this.setState({
-            clickedBlue : true
-        });
-    }
+  clickedBlueColor = () => {
+    this.setState({
+      clickedBlue: true
+    });
+  };
 
-    clickedGreenColor = () =>{
-        this.setState({
-            clickedGreen : true
-        });
-    }
+  clickedGreenColor = () => {
+    this.setState({
+      clickedGreen: true
+    });
+  };
 
-    clickedYellowColor = () =>{
-        this.setState({
-            clickedYellow : true
-        });
-    }
+  clickedYellowColor = () => {
+    this.setState({
+      clickedYellow: true
+    });
+  };
 
+  //ColoreTabToggle
 
-    //ColoreTabToggle
+  colorTabToggle = () => {
+    const currentState = this.state.showColorTab;
+    this.setState({
+      showColorTab: !currentState
+    });
+  };
 
-    colorTabToggle = () => {
-
-        
-        const currentState = this.state.showColorTab;
-        this.setState({
-        
-         showColorTab: !currentState
-         
-
-        });
-       
-        
-    }
-
-    render(){
-
-        
-            const backgrounColor = [styles.cardBody];
-            const nameColor = [styles.textFix];
-            if(this.state.clickedRed){
-                backgrounColor.push(styles.red);
-                nameColor.push(styles.textred);
-                this.state.clickedRed = false;
-            }
-            if(this.state.clickedBlue){
-                backgrounColor.push(styles.blue);
-                nameColor.push(styles.textblue);
-                this.state.clickedBlue = false;
-            }
-            if(this.state.clickedGreen){
-                backgrounColor.push(styles.green);
-                nameColor.push(styles.textgreen);
-                this.state.clickedGreen = false;
-            }
-            if(this.state.clickedYellow){
-                backgrounColor.push(styles.yellow);
-                nameColor.push(styles.textyellow);
-               this.state.clickedYellow = false;
-            }
-        
-            
-            
-        const classr = [styles.box, styles.red];
-        const classb = [styles.box, styles.blue];
-        const classg = [styles.box, styles.green];
-        const classy = [styles.box, styles.yellow];
-        
-        return(
-            <div>
-            
-           
-            {
-            this.state.showColorTab? 
-            
-            <div className = {styles.middle}>
-            
-            <div className = {classr.join(' ')} onClick = {this.clickedRedColor}></div>
-            <div className = {classb.join(' ')} onClick = {this.clickedBlueColor}></div>
-            <div className = {classg.join(' ')} onClick = {this.clickedGreenColor}></div>
-            <div className = {classy.join(' ')} onClick = {this.clickedYellowColor}></div>
-            </div> :
-             null}
-            <CHead bodyMessage = {this.state.bodyMessage} name= {this.state.name} color = {backgrounColor} nameColor = {nameColor}/>
-            <FontAwesomeIcon icon="ellipsis-h"  className={styles.ellipPosition} onClick = {this.colorTabToggle} />
-            <CFoot footerMessage = {this.state.footerMessage} footerHeading = {this.state.footerHeading}/>
-            
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CHead
+          bodyMessage={this.state.bodyMessage}
+          name={this.state.name}
+          clickedRed = {this.state.clickedRed}
+          clickedBlue= {this.state.clickedBlue}
+          clickedGreen= {this.state.clickedGreen}
+          clickedYellow= {this.state.clickedYellow}
+          clickedRedColor ={this.state.clickedRedColor}
+          clickedBlueColor ={this.state.clickedBlueColor}
+          clickedGreenColor ={this.state.clickedGreenColor}
+          clickedYellowColor ={this.state.clickedYellowColor}
+          
+        />
+        <FontAwesomeIcon
+          icon="ellipsis-h"
+          className={styles.ellipPosition}
+          onClick={this.colorTabToggle}
+        />
+        <CFoot
+          footerMessage={this.state.footerMessage}
+          footerHeading={this.state.footerHeading}
+        />
+      </div>
+    );
+  }
 }
 
 export default Card;
--- src/CardHeader/CardHeader.js
+++ src/CardHeader/CardHeader.js
@@ -6,31 +6,74 @@ import styles from '../CardHeader/CardHeader.css';
 
 const card = (props) =>{
  
-   
+            const backgrounColor = [styles.cardBody];
+            const nameColor = [styles.textFix];
+            if(props.clickedRed){
+                backgrounColor.push(styles.red);
+                nameColor.push(styles.textred);
+                
+            }
+            if(props.clickedBlue){
+                backgrounColor.push(styles.blue);
+                nameColor.push(styles.textblue);
+               
+            }
+            if(props.clickedGreen){
+                backgrounColor.push(styles.green);
+                nameColor.push(styles.textgreen);
+                
+            }
+            if(props.clickedYellow){
+                backgrounColor.push(styles.yellow);
+                nameColor.push(styles.textyellow);
+               
+            }
+
+            const classr = [styles.box, styles.red];
+        const classb = [styles.box, styles.blue];
+        const classg = [styles.box, styles.green];
+        const classy = [styles.box, styles.yellow];
 
    
     //const colorActive = '';
     
    
       return(
+        
+
+        
+
+
         <div className = {styles.cardContainer}>
-    
+            
+        {/* color code */}  
+
+     
+            
+            <div className = {styles.middle}>
+            
+            <div className = {classr.join(' ')} onClick = {props.clickedRedColor}></div>
+            <div className = {classb.join(' ')} onClick = {props.clickedBlueColor}></div>
+            <div className = {classg.join(' ')} onClick = {props.clickedGreenColor}></div>
+            <div className = {classy.join(' ')} onClick = {props.clickedYellowColor}></div>
+            </div> 
+            
         
 
 
     
         
-        <div className = {props.color.join(' ')}>
+        <div className = {backgrounColor.join(' ')}>
             
             <div className = {styles.box_main}>
                 DEV
             </div>
             
-       
-        <textarea name='text' rows='5' cols = '80' wrap ='soft'></textarea>
+        <h1>{props.bodyMessage}</h1>
+        
 
         <div className={styles.side_box}>
-           <p1 className={props.nameColor.join(' ')}>{props.name}</p1> 
+           <p1 className={nameColor.join(' ')}>{props.name}</p1> 
            <div className= {styles.imageSpace}> 
           
            </div>
